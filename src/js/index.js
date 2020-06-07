3fb1b9b5fcba44c8bddf60fb2f63fe748c7976eb
Łukasz Mateusz Granica <lukasz.granica@banqsoft.pl>
2020-06-07 13:17:47 +0200

    implement dark mode on site

--- src/js/index.js
+++ src/js/index.js
@@ -6,3 +6,19 @@ import moment from 'moment';
 
 console.log(moment().format('MMMM Do YYYY, h:mm:ss a'));
 
+const checkbox = document.querySelector('.darkmode--js');
+
+checkbox.addEventListener('change', () => {
+    if(checkbox.checked) {
+        document.documentElement.style.setProperty('--backgroud','#746d75')
+        document.documentElement.style.setProperty('--navBackground','#2a324b')
+        document.documentElement.style.setProperty('--color','#e1e5ee')
+        document.documentElement.style.setProperty('--mainHeaderColor','#c7ccdb')
+      } else {
+        document.documentElement.style.setProperty('--backgroud','#92ce92')
+        document.documentElement.style.setProperty('--navBackground','#1d9627')
+        document.documentElement.style.setProperty('--color','#333')
+        document.documentElement.style.setProperty('--mainHeaderColor','#7d1d3f')
+      }
+})
+
