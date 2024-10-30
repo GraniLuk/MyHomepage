491399dca4738b375a4d653575b3945fd0742dda
Łukasz Mateusz Granica <granica.lukasz@gmail.com>
2024-10-30 09:10:10 +0100

    Set backgroud to white

--- src/js/index.js
+++ src/js/index.js
@@ -18,8 +18,8 @@ document.addEventListener('DOMContentLoaded', () => {
         document.documentElement.style.setProperty('--color','#e1e5ee');
         document.documentElement.style.setProperty('--mainHeaderColor','#c7ccdb');
       } else {
-        document.documentElement.style.setProperty('--backgroud','#92ce92');
-        document.documentElement.style.setProperty('--navBackground','#1d9627');
+        document.documentElement.style.setProperty('--backgroud','#fffff');
+        document.documentElement.style.setProperty('--navBackground','#fffff');
         document.documentElement.style.setProperty('--color','#333');
         document.documentElement.style.setProperty('--mainHeaderColor','#7d1d3f');
       }
