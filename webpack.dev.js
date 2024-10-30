57e51ebbccf1a838164214771f1678273570c102
Łukasz Mateusz Granica <granica.lukasz@gmail.com>
2024-10-30 20:26:04 +0100

    Fix development.

--- webpack.dev.js
+++ webpack.dev.js
@@ -3,15 +3,22 @@ const common = require("./webpack.common.js");
 const { merge } = require('webpack-merge');
 
 module.exports = merge(common, {
-  devtool: "source-map",
+  devtool: "inline-source-map",
   mode: "development",
 
   devServer: {
     port: 8080,
     static: {
-      directory: path.join(__dirname, 'src'),
+      directory: path.join(__dirname, 'dist'),
     },
     historyApiFallback: true, // Optional, if using React Router or similar
     hot: true, // Enables Hot Module Replacement
+    open: true,
+    devMiddleware: {
+      writeToDisk: true,  // This will write files to disk in dev mode
+    },
+  },
+  output: {
+    publicPath: '/',  // This is important for dev server
   },
 });
